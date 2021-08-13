85c1cf03c38b0af00e37591ac41d8e6984d8bfa1
Juan Pablo Ortega <jortegab@unal.edu.co>
2021-08-13 12:52:49 -0500

    Pro stuff added

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky")
+        }else{
+            $('.navbar').removeClass("sticky")
+        }
+        
+    })
+    
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    var typed = new Typed(".typing",{
+        strings: ["Mathematician","Computational Scientist","Researcher","Teacher","Programmer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+
+});

73707ccb947a54c4e9b891087c4bcfcdf2acc831
User <jortegab@unal.edu.co>
2022-05-11 14:17:45 -0500

    2022 big actualization

--- script.js
+++ script.js
@@ -14,10 +14,37 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing",{
-        strings: ["Mathematician","Computer Scientist","Teacher","Researcher","Programmer"],
+        strings: ["Mathematician","Data Scientist","Researcher","Business Analist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
-    })
+    });
 
+    var typed = new Typed(".typing2",{
+        strings: ["Mathematician","Data Scientist","Researcher","Business Analist"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    //owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            820:{
+                items: 2,
+                nav: false
+            },
+            1250:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
